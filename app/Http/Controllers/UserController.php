d8e1578a5568e08ccc4d3d6dff86843771264910
Raginee Gupta <raginee123@gmail.com>
2022-12-08 14:28:38 +0530

    connect with Database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,22 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    function loadView()
-    {
-        $data=['Raag','Rohit','Amit','Vikas','Abhay'];
-        return view("users",["users"=>$data]);
+    // function loadView()
+    // {
+    //     $data=['Raag','Rohit','Amit','Vikas','Abhay'];
+    //     return view("users",["users"=>$data]);
 
-    }
+    // }
 
-    function getData(Request $req)
-    {
-        $req->validate([
-            'username'=>'required | max:10',
-            'password'=>'required | min:5'
-        ]);
-        return $req->input();
+    // function getData(Request $req)
+    // {
+    //     $req->validate([
+    //         'username'=>'required | max:10',
+    //         'password'=>'required | min:5'
+    //     ]);
+    //     return $req->input();
+    // }
+
+    function index(){
+        return DB::select("select * from users");
     }
 }
