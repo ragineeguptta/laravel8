a3e7c2d7bc4f6314d6435e7895f88e439b141558
Raginee Gupta <raginee123@gmail.com>
2022-12-12 11:41:57 +0530

    stub

---
+++ app/Http/Controllers/stubController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class stubController extends Controller
+{
+    //
+    function index(Request $req){
+        return "some code";
+    }
+}
